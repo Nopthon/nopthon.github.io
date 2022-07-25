a2b6b37f62167c24bdc903b223296b0431da95ac
Nopthon <61966151+Nopthon@users.noreply.github.com>
2022-07-25 21:21:27 +0800

    Delete check.js

--- check.js
+++
@@ -1,9 +0,0 @@
-function check() {
-var a= 72
-var input= document.getElementById("number").value
-	if (a==input){
-	alert("You found one egg!")
-	} else{
-	alert("Thanks for your feedback, but we won't see it lol!")
-	}
-}
